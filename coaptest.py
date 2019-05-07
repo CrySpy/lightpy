c4fb524f905fee85d70a45c67183b65f658ab564
CrySpy1 <cryspy@cryspy.no>
2019-05-07 13:51:34 +0200

    init

---
+++ coaptest.py
@@ -0,0 +1,21 @@
+from coapthon.client.helperclient import HelperClient
+import time
+
+host = "fd00::212:4b00:c46:9280"
+port = 5683
+path ="sensors/light"
+
+
+client = HelperClient(server=(host, port))
+
+while True:
+    response = client.get(path)
+    print(response.source[0])
+    print(response.payload)
+    time.sleep(10)
+
+
+
+
+#print(response.pretty_print())
+client.stop()
