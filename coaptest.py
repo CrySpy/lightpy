0502dc87689d593e5545c9b2552abdd97bd5ed74
CrySpy1 <cryspy@cryspy.no>
2019-05-09 04:02:18 +0200

    Added logging to text file

--- coaptest.py
+++ coaptest.py
@@ -1,5 +1,6 @@
 from coapthon.client.helperclient import HelperClient
 import time
+from datetime import datetime as dt
 
 host = "fd00::212:4b00:c46:9280"
 port = 5683
@@ -12,6 +13,8 @@ while True:
     response = client.get(path)
     print(response.source[0])
     print(response.payload)
+    with open("9280.log", "a") as log:
+        log.write("{}\t{}\t{}".format(response.source[0], dt.now().strftime("%Y-%m-%d %H:%M:%S"), response.payload))
     time.sleep(120)
 
 
